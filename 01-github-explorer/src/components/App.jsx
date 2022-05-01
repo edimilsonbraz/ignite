78e6c121c456eebd31d7ea7c85f718bd4b20daba
edimilsonbraz <edimilson.gt8@gmail.com>
2022-05-01 08:30:06 -0300

    :alien: Consumindo API / useState / useEffect

--- 01-github-explorer/src/components/App.jsx
+++ 01-github-explorer/src/components/App.jsx
@@ -1,12 +1,9 @@
 import '../styles/global.scss';
+
 import { RepositoryList } from './RepositoryList'
-import { Counter } from './Counter'
 
 export function App() {
   return (
-    <>
-      <RepositoryList />
-      <Counter />
-    </>
+    <RepositoryList />
   )
 }
