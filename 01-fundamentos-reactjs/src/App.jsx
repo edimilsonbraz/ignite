416d4d0892200f20c49a01c0e902dee31417eae0
edimilsonbraz <edimilsonbraz@pop-os.localdomain>
2022-09-30 10:07:02 -0300

    :tada: First Commit Project
    Esse foi o primeiro commit

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,20 @@
+import { Post } from './Post';
+
+export function App() {
+  return (
+    <div>
+      <Post 
+        author="Edimilson Braz" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing"
+      />
+      <Post 
+        author="Diego Fernandes" 
+        content="Lorem ipsum dolor "
+      />
+     
+    </div>
+  )
+}
+
+ 
+ 
