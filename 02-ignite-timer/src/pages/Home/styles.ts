cfcda079566da426e884badfca9bb99974fad24c
edimilson braz <edimilson.gt8@gmail.com>
2022-10-13 15:21:16 -0300

    :recycle: Refatoracao / ContextProvider OK

--- 02-ignite-timer/src/pages/Home/styles.ts
+++ 02-ignite-timer/src/pages/Home/styles.ts
@@ -2,12 +2,10 @@ import styled from 'styled-components'
 
 export const HomeContainer = styled.main`
   flex: 1;
-
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
   form {
     display: flex;
     flex-direction: column;
@@ -21,19 +19,13 @@ export const BaseCountdownButton = styled.button`
   border: 0;
   padding: 1rem;
   border-radius: 8px;
-
   display: flex;
   align-items: center;
   justify-content: center;
   color: ${(props) => props.theme['gray-100']};
-
   gap: 0.5rem;
   font-weight: bold;
-
   cursor: pointer;
-
-  background: ${(props) => props.theme['green-500']};
-
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
@@ -42,7 +34,7 @@ export const BaseCountdownButton = styled.button`
 
 export const StartCountdownButton = styled(BaseCountdownButton)`
   background: ${(props) => props.theme['green-500']};
-
+  color: ${(props) => props.theme['gray-100']};
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
   }
@@ -50,7 +42,6 @@ export const StartCountdownButton = styled(BaseCountdownButton)`
 
 export const StopCountdownButton = styled(BaseCountdownButton)`
   background: ${(props) => props.theme['red-500']};
-
   &:not(:disabled):hover {
     background: ${(props) => props.theme['red-700']};
   }
