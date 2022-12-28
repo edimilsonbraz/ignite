8d515d9ab475c4421747fbb222527f09100579d7
edimilson braz <edimilson.gt8@gmail.com>
2022-12-28 07:43:20 -0300

    :recycle: Refatoracao de Componentes

--- desafios/03-github-blog/src/pages/Blog/components/SearchForm/index.tsx
+++ desafios/03-github-blog/src/pages/Blog/components/SearchForm/index.tsx
@@ -1,11 +1,14 @@
+import { IssuesProps } from "../..";
 import { SearchFormContainer, SearchFormText } from "./styles";
 
-export function SearchForm() {
+export function SearchForm(issues: IssuesProps[]) {
+  console.log(issues)
+  
   return (
     <SearchFormContainer className="container">
       <SearchFormText>
         <h3>Publicações</h3>
-        <span>6 publicações</span>
+        <span>{issues.length} publicações</span>
       </SearchFormText>
 
       <input
