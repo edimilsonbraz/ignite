d05f04659cd8919210e83b0b2faf3bd67b6847ad
edimilson braz <edimilson.gt8@gmail.com>
2023-01-03 15:39:07 -0300

    :tada: Iniciando o projeto Ignite Shop

---
+++ aulas/04-ignite-shop/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = {
+  experimental: {
+    fontLoaders: [
+      {loader: '@next/font/google', options: { subsets: ['latin'] }},
+    ],
+  },
+}
