b919b9f97d0523828c18fde3378a79a83f4168cc
edimilson braz <edimilson.gt8@gmail.com>
2023-01-04 15:47:44 -0300

    :wrench: Aplicando estilos globais com stitches

--- aulas/04-ignite-shop/src/pages/index.tsx
+++ aulas/04-ignite-shop/src/pages/index.tsx
@@ -1,8 +1,33 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$green300',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+  color: 'white',
+  cursor: 'pointer',
+  transition: '0.3s',
+
+  span: {
+    fontWeight: 'bold',
+    color: '#333',
+    marginRight: 6,
+  }, 
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  },
+
+})
 
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Button>
+        <span>Teste</span>
+        Enviar
+      </Button>
     </>
   )
 }
