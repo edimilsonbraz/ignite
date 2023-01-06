94691e80b79f8e945fa147ab1fdebb85671cbc93
edimilson braz <edimilson.gt8@gmail.com>
2023-01-06 09:30:41 -0300

    :alien: Buscando produtos do Stripe

--- aulas/04-ignite-shop/next.config.js
+++ aulas/04-ignite-shop/next.config.js
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = {
+  images: {
+    domains: [
+      'files.stripe.com',
+    ]
+  },
+
   experimental: {
     fontLoaders: [
       {loader: '@next/font/google', options: { subsets: ['latin'] }},
     ],
   },
+
 }
+
+module.exports = nextConfig
