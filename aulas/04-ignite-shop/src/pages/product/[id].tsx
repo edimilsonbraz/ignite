32a8f58515f44b79f43f9d54cfaefeedf85f4dd0
edimilson braz <edimilson.gt8@gmail.com>
2023-01-09 13:53:50 -0300

    :sparkles: Estrutura da pagina de produto

--- aulas/04-ignite-shop/src/pages/product/[id].tsx
+++ aulas/04-ignite-shop/src/pages/product/[id].tsx
@@ -1,9 +1,34 @@
-import { useRouter } from "next/router"
+import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails
+} from '../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camisa X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias ex
+          recusandae, provident perferendis maxime fugiat architecto commodi
+          repellat est corrupti adipisci aliquid perspiciatis consequuntur sed
+          optio temporibus, tempore quasi? Alias.
+        </p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
