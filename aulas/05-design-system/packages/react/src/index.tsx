75e5001782eab40b3adecb0a2838a36c7f67cb0b
edimilson braz <edimilson.gt8@gmail.com>
2023-01-27 16:34:45 -0300

    :arrow_up: instalando e config Stitches/react

--- aulas/05-design-system/packages/react/src/index.tsx
+++ aulas/05-design-system/packages/react/src/index.tsx
@@ -1,4 +1,11 @@
-import { colors } from '@ignite-ui/tokens'
+import { styled } from "./styles"
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  background: '$gray500',
+  borderRadius: '$md',
+  padding: '$4',
+})
 
 export function App() {
   return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
