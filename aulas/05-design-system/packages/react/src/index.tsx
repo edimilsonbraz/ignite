14aad0526aea45b1997fc7302c29bea9521cfc10
edimilson braz <edimilson.gt8@gmail.com>
2023-01-31 08:47:22 -0300

    :sparkles: Instalando e criando App Storybook

--- aulas/05-design-system/packages/react/src/index.tsx
+++ aulas/05-design-system/packages/react/src/index.tsx
@@ -1,12 +1,12 @@
-import { styled } from "./styles"
+import { styled } from './styles'
 
 const Button = styled('button', {
   fontFamily: '$default',
-  background: '$gray500',
+  background: '$ignite500',
   borderRadius: '$md',
   padding: '$4',
 })
 
 export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+  return <Button>Hello World</Button>
 }
