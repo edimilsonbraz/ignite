14b334c9e00df5973a0628c75effbf6e4a9e85f9
Edimilson BrazZ <edimilson.gt8@gmail.com>
2024-04-25 18:38:37 -0300

    Delete aulas/desafios directory

--- aulas/desafios/03-github-blog/src/pages/Blog/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const CardListContainer = styled.section`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 2rem;
-
-  margin-bottom: 6rem;
-`
